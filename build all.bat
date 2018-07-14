c8d9bbd43b8afade7d360acd23d2e6b44d250d6e
infeeeee <gyetpet@gmail.com>
2018-07-14 21:00:29 +0200

    fixed build all.bat

--- build all.bat
+++ build all.bat
@@ -3,14 +3,14 @@ set dt=%dt:~0,8%
 echo %dt%
 set startdir=%~dp0
 cd %~dp0
-md .\builds\%dt%-all .\builds\%dt%-all\macos .\builds\%dt%-all\win\bat .\builds\%dt%-all\linux
-copy .\settings.ini.example .\builds\%dt%-all\macos\settings.ini
-copy .\settings.ini.example .\builds\%dt%-all\win\settings.ini
-copy .\settings.ini.example .\builds\%dt%-all\linux\settings.ini
-copy .\bat\* .\builds\%dt%-all\win\bat
+md .\builds\%dt%-all .\builds\%dt%-all\kimai-cmd-macos64 .\builds\%dt%-all\kimai-cmd-win64\bat .\builds\%dt%-all\kimai-cmd-linux64
+copy .\settings.ini.example .\builds\%dt%-all\kimai-cmd-macos64\settings.ini
+copy .\settings.ini.example .\builds\%dt%-all\kimai-cmd-win64\settings.ini
+copy .\settings.ini.example .\builds\%dt%-all\kimai-cmd-linux64\settings.ini
+copy .\bat\* .\builds\%dt%-all\kimai-cmd-win64\bat
 start "" /w cmd /c pkg --out-path .\builds\%dt%-all .\kimai-cmd.js
 cd .\builds\%dt%-all
-move kimai-cmd-linux linux\kimai-cmd
-move kimai-cmd-macos macos\kimai-cmd
-move kimai-cmd-win.exe win\kimai-cmd.exe
+move kimai-cmd-linux kimai-cmd-linux64\kimai-cmd
+move kimai-cmd-macos kimai-cmd-macos64\kimai-cmd
+move kimai-cmd-win.exe kimai-cmd-win64\kimai-cmd.exe
 cd %startdir%
